3cc911f59e2d3153e74635d33b62c38dea78272e
pattan <patrik.wilhelmsson@minmb.se>
2024-05-08 08:58:29 +0200

    docs: favicon

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -12,7 +12,17 @@ export default defineConfig({
     publicDir: "public",
     head: [
         ['script', { src: 'https://cdn.tailwindcss.com' }],
-        ['link', { href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap', rel: 'stylesheet' }]
+        ['link', { href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap', rel: 'stylesheet' }],
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png" }],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png" }],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png" }],
+        ['link', { rel: "manifest", href: "/favicons/site.webmanifest" }],
+        ['link', { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#6b04fd" }],
+        ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico" }],
+        ['meta', { name: "msapplication-TileColor", content: "#6b04fd" }],
+        ['meta', { name: "msapplication-config", content: "/favicons/browserconfig.xml" }],
+        ['meta', { name: "theme-color", content: "#18181b", media: "(prefers-color-scheme: dark)" }],
+        ['meta', { name: "theme-color", content: "#FFFFFF", media: "(prefers-color-scheme: light)" }],
     ],
     markdown: {
         theme: {
